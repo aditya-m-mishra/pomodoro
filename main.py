ce1811d10f46e62c5695e444cde093979d51ef6b
aditya-m-mishra <adiapplies@gmail.com>
2026-04-21 18:08:10 +0530

    dynamic typing added to count_down func

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(300)
+    count_down(15)
 
 
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
