da1641fa18c3160f5f0733a3bce90f7df39faae4
aditya-m-mishra <adiapplies@gmail.com>
2026-04-21 18:40:00 +0530

    project completed

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 # constants over
 
 reps = 0
+timer = NONE
 
 
 def start_timer():
@@ -24,10 +25,14 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
+        title_label.config(text="break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
+        title_label.config(text="break", fg=PINK)
+
     else:
         count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 def count_down(count):
@@ -38,9 +43,24 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        chech_marks.config(text=marks)
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    chech_marks.config(text="")
+    global reps
+    reps = 0
 
 
 window = Tk()
@@ -61,10 +81,10 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-chech_marks = Label(text="✓", font=(40), fg=GREEN, bg=YELLOW)
+chech_marks = Label(font=(40), fg=GREEN, bg=YELLOW)
 chech_marks.grid(column=1, row=3)
 
 
